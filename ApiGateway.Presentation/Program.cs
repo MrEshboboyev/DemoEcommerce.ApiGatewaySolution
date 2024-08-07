52f198937f5922822c3af516752f43e5e577318c
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-07 06:26:21 +0500

    Program.cs : Configure ocelot, AddJwtAuthenticationScheme(), AddCors(), UseCors(); Adding Project Reference : Presentation + SharedLibrary;

--- ApiGateway.Presentation/Program.cs
+++ ApiGateway.Presentation/Program.cs
@@ -1,6 +1,24 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using eCommerce.SharedLibrary.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
+JWTAuthenticationScheme.AddJWTAuthenticationScheme(builder.Services, builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
 
+app.UseCors();
 app.UseHttpsRedirection();
 app.Run();
